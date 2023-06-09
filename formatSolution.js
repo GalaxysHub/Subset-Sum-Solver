2b7eadc13cf27d449ced2dde9065bdc38320a0d9
Andrew <andrew@Andrews-MacBook-Air.local>
2023-06-08 23:46:36 -0500

    found best solution and removed special cases from js script

--- formatSolution.js
+++ formatSolution.js
@@ -8,7 +8,6 @@ let formatedSolution = {};
 async function run() {
   let rawData = await fs.readFileSync("solution.json");
   let solution = JSON.parse(rawData);
-  // await specialCases(solution);
   await formatSolution(solution);
   await writeSolution();
 }
@@ -26,68 +25,6 @@ function sortAlphaNum(a, b) {
   } else {
     return aA > bA ? 1 : -1;
   }
-}
-
-async function specialCases(solution) {
-  const { Sections, CampAreas, Camps, CampLengths } = solution;
-
-  Sections.forEach(async (section, i) => {
-    if (section === "C32") {
-      //add Camp Schwifty 2020!
-      Camps[i].push("Camp Schwifty 2020!");
-      CampAreas[i].push(CampAreasMap["Camp Schwifty 2020!"]);
-      CampLengths[i].push(Math.round(CampAreasMap["Camp Schwifty 2020!"] / 70));
-      //Add M3ga St3llar Ali3ns
-      Camps[i].push("M3ga St3llar Ali3ns");
-      CampAreas[i].push(CampAreasMap["M3ga St3llar Ali3ns"]);
-      CampLengths[i].push(Math.round(CampAreasMap["M3ga St3llar Ali3ns"] / 70));
-    } else if (section === "B32") {
-      // add slippery saucy sloots
-      Camps[i].push("slippery saucy sloots");
-      CampAreas[i].push(CampAreasMap["slippery saucy sloots"]);
-      CampLengths[i].push(
-        Math.round(CampAreasMap["slippery saucy sloots"] / 70)
-      );
-      //Add cabbage pash kids`
-      Camps[i].push("cabbage pash kids");
-      CampAreas[i].push(CampAreasMap["cabbage pash kids"]);
-      CampLengths[i].push(Math.round(CampAreasMap["cabbage pash kids"] / 70));
-    }
-    const CampNames = Camps[i];
-    CampNames.forEach((campName, j) => {
-      if (campName === "Camp Ohana x2") {
-        //separate 2 camp ohanas
-        Camps[i].splice(j, 1, "Ohana 1", "Ohana 2");
-        CampAreas[i].splice(
-          j,
-          1,
-          CampAreasMap["Camp Ohana 1"],
-          CampAreasMap["Camp Ohana 2"]
-        );
-        CampLengths[i].splice(
-          j,
-          1,
-          Math.round(CampAreasMap["Camp Ohana 1"] / 70),
-          Math.round(CampAreasMap["Camp Ohana 2"] / 70)
-        );
-      } else if (campName === "TEAM BLAST OFF & Camp Wurder") {
-        //seprate Team Blast Off and Camp Wurder
-        Camps[i].splice(j, 1, "TEAM BLAST OFF", "Camp Wurder");
-        CampAreas[i].splice(
-          j,
-          1,
-          CampAreasMap["TEAM BLAST OFF"],
-          CampAreasMap["Camp Wurder"]
-        );
-        CampLengths[i].splice(
-          j,
-          1,
-          Math.round(CampAreasMap["TEAM BLAST OFF"] / 70),
-          Math.round(CampAreasMap["Camp Wurder"] / 70)
-        );
-      }
-    });
-  });
 }
 
 async function formatSolution(solution) {
